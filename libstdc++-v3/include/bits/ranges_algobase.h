8b254e95fc9308ae931ec5eae8739d724b5c2ff3
Michael Levine <mlevine55@bloomberg.net>
2024-05-16 16:05:00 -0400

    Added the missing out_value_result
    Signed-off-by: Michael Levine <mlevine55@bloomberg.net>

--- libstdc++-v3/include/bits/ranges_algobase.h
+++ libstdc++-v3/include/bits/ranges_algobase.h
@@ -71,6 +71,32 @@ namespace ranges
 	__is_move_iterator<move_iterator<_Iterator>> = true;
   } // namespace __detail
 
+#if __glibcxx_ranges_iota >= 202202L // C++ >= 23
+
+    template<typename _Out, typename _Tp>
+    struct out_value_result
+    {
+        [[no_unique_address]] _Out out;
+        [[no_unique_address]] _Tp value;
+
+        template<typename _Out2, typename _Tp2>
+	requires convertible_to<const _Out&, _Out2>
+        && convertible_to<const _Tp&, _Tp2>
+	constexpr
+	operator out_value_result<_Out2, _Tp2>() const &
+	{ return {out, value}; }
+
+        template<typename _Out2, typename _Tp2>
+	requires convertible_to<_Out, _Out2>
+        && convertible_to<_Tp, _Tp2>
+	constexpr
+	operator out_value_result<_Out2, _Tp2>() &&
+	{ return {std::move(out), std::move(value)}; }
+    };
+
+#endif // __glibcxx_ranges_iota
+
+
   struct __equal_fn
   {
     template<input_iterator _Iter1, sentinel_for<_Iter1> _Sent1,
